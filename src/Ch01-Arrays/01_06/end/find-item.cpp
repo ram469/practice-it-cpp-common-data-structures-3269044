4e46c003ca9ac3c4103b7fdeec2c691b42362bed
Ramakrishna Mandalapu <ram.physics69@gmail.com>
2025-11-25 08:54:32 +0000

    find item

--- src/Ch01-Arrays/01_06/end/find-item.cpp
+++ src/Ch01-Arrays/01_06/end/find-item.cpp
@@ -5,10 +5,21 @@ using namespace std;
 template <typename T>
 int findItem(const T arr[], size_t size, T target)
 {
-    // TODO: implement
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main()
 {
+    float numbers[]{4.0, 7, 12, 3.1, 3.2, 17, 3.5};
+    size_t size = sizeof(numbers) / sizeof(numbers[0]);
+    float target = 3.2;
+    cout << "The index of " << target << " is " << findItem(numbers, size, target) << " (zero indexing)" << endl;
     return 0;
 }
